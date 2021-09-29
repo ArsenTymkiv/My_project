53e2ec3d2644b02930cfc8237f46a6d371500bed
Arsen <arseny.tymkiv.ua@gmail.com.com>
2021-09-29 14:42:53 +0300

    Add categories posts and add categories product

--- wp-content/themes/tema1/new.php
+++ wp-content/themes/tema1/new.php
@@ -3,13 +3,16 @@
  * Template Name: Custom
  */
 get_header(); ?>
+    <div class="text_post">
+        <h2><strong>Категорії постів:</strong><h2/>
 
-
+    </div>
 
 <?php
+/////////Категорії постів
 $taxonomy = 'category'; // Here, you can pass custom taxonomy also.
 $args = array(
-    'taxonomy' => $taxonomy
+    'taxonomy' => $taxonomy,
 );
 
 // Get All categories.
@@ -18,11 +21,206 @@ $categories = get_categories($args);
 
 foreach($categories as $category) {
     echo '<li>
-            <a href="' . get_term_link($category) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . '>' . $category->name . '</a>
+            <a href="' . get_term_link($category) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . '>' . $category->name . '</a>   
          </li> ';
+    echo '<p>'. $category->description .'</p>';
+}
+?>
 
+
+
+
+
+
+
+
+
+
+
+
+    <div class="text_post">
+        <h2><strong>Категорії товарів:</strong><h2/>
+
+    </div>
+<?php
+/////////Категорії товарів
+
+
+$cat_args = array(
+    'orderby'    => 'name',
+    'order'      => 'asc',
+);
+
+$product_categories = get_terms( 'product_cat', $cat_args );
+var_dump($product_categories);
+if( !empty($product_categories) ){
+    echo '
+ 
+<ul>';
+    foreach ($product_categories as $key => $category) {
+        echo '
+ 
+<li>';
+        echo '<a href="'.get_term_link($category).'" >';
+        echo $category->name;
+        echo '</a>';
+        echo '</li>';
+    }
+    echo '</ul>
+ 
+ 
+';
 }
 
+
+/*
+ $args = array(
+    'post_type' => 'page'
+);
+$query = new WP_Query;
+$my_posts = $query->query($args);
+
+foreach( $my_posts as $my_post ){
+    echo '<p>'. $my_post->post_title .'</p>';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+<?php
+echo '<br><br/>';
+echo '<br><br/>';
+$my_posts = new WP_Query;
+$myposts = $my_posts->query( array(
+    'posts_per_page' => 2,
+    'category_name' => 'TESTCAT'
+) );
+while ( $my_posts->have_posts() ) {
+    $my_posts->the_post();
+
+    the_title("....."); // выведем заголовок поста
+}
+// Возвращаем оригинальные данные поста. Сбрасываем $post.
+wp_reset_postdata();
+
+
+
+
+
+
+
+echo '<br><br/>';
+echo '<br><br/>';
+// создаем экземпляр
+$my_posts = new WP_Query;
+
+// делаем запрос
+$myposts = $my_posts->query( array(
+    'post_type' => 'page'
+) );
+
+// обрабатываем результат
+foreach( $myposts as $pst ){
+    echo " ".esc_html( $pst->post_title );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo '<br><br/>';
+echo '<br><br/>';
+
+
+
+
+// задаем нужные нам критерии выборки данных из БД
+$args = array(
+    'posts_per_page' => 6,
+    'order by' => 'comment_count'
+);
+
+$query = new WP_Query( $args, [ 'category_name' => 'TEST' ] );
+
+// Цикл
+if ( $query->have_posts() ) {
+    while ( $query->have_posts() ) {
+        $query->the_post();
+        echo '<li>' . get_the_title() . '</li>';
+    }
+} else {
+    // Постов не найдено
+}
+// Возвращаем оригинальные данные поста. Сбрасываем $post.
+wp_reset_postdata();
+
+
+
+
+
+
+
+
+
+
+
+echo '<br><br/>';
+echo '<br><br/>';
+
+// запрос
+$query = new WP_Query( $args ); ?>
+
+<?php if ( $query->have_posts() ) : ?>
+
+    <!-- пагинация -->
+
+    <!-- цикл -->
+    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+    <?php endwhile; ?>
+    <!-- конец цикла -->
+
+    <!-- пагинация -->
+
+    <?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+    <p><?php esc_html_e( 'Нет постов по вашим критериям.' ); ?></p>
+<?php endif; ?>
+
+ <?php
+
+
+
+
+
+
+
+
+
+
+*/
 ?>
 
 
